5f604dfc885359fcac33d9020b28d7b7f0e0fca2
Kypsis <puhhbaer@hotmail.com>
2022-07-30 02:53:12 +0300

    initial commit

---
+++ lib/src/win_game/win_game_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../ads/ads_controller.dart';
+import '../ads/banner_ad_widget.dart';
+import '../games_services/score.dart';
+import '../in_app_purchase/in_app_purchase.dart';
+import '../style/palette.dart';
+import '../style/responsive_screen.dart';
+
+class WinGameScreen extends StatelessWidget {
+  final Score score;
+
+  const WinGameScreen({
+    super.key,
+    required this.score,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final adsControllerAvailable = context.watch<AdsController?>() != null;
+    final adsRemoved = context.watch<InAppPurchaseController?>()?.adRemoval.active ?? false;
+    final palette = context.watch<Palette>();
+
+    const gap = SizedBox(height: 10);
+
+    return Scaffold(
+      backgroundColor: palette.backgroundPlaySession,
+      body: ResponsiveScreen(
+        squarishMainArea: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (adsControllerAvailable && !adsRemoved) ...[
+              const Expanded(
+                child: Center(
+                  child: BannerAdWidget(),
+                ),
+              ),
+            ],
+            gap,
+            const Center(
+              child: Text(
+                'You won!',
+                style: TextStyle(fontFamily: 'Permanent Marker', fontSize: 50),
+              ),
+            ),
+            gap,
+            Center(
+              child: Text(
+                'Score: ${score.score}\n'
+                'Time: ${score.formattedTime}',
+                style: const TextStyle(fontFamily: 'Permanent Marker', fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+        rectangularMenuArea: ElevatedButton(
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+          child: const Text('Continue'),
+        ),
+      ),
+    );
+  }
+}
