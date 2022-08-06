c95a3e1feafb215e2540273ae10fac71892e5497
Kypsis <puhhbaer@hotmail.com>
2022-08-07 00:37:10 +0300

    extract router

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,12 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:game_template/router.dart';
 import 'package:game_template/src/audio/audio_controller.dart';
 import 'package:game_template/src/in_app_purchase/ad_removal_state.gen.dart';
 import 'package:game_template/src/player_progress/player_progress.dart';
 import 'package:game_template/src/settings/settings.dart';
 import 'package:game_template/src/settings/settings_state.gen.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
@@ -24,19 +23,11 @@ import 'package:logging/logging.dart';
 import 'src/ads/ads_controller.dart';
 import 'src/crashlytics/crashlytics.dart';
 import 'src/games_services/games_services.dart';
-import 'src/games_services/score.dart';
 import 'src/in_app_purchase/in_app_purchase.dart';
-import 'src/level_selection/level_selection_screen.dart';
-import 'src/level_selection/levels.dart';
-import 'src/main_menu/main_menu_screen.dart';
-import 'src/play_session/play_session_screen.dart';
 import 'src/player_progress/persistence/local_storage_player_progress_persistence.dart';
 import 'src/settings/persistence/local_storage_settings_persistence.dart';
-import 'src/settings/settings_screen.dart';
-import 'src/style/my_transition.dart';
 import 'src/style/palette.dart';
 import 'src/style/snack_bar.dart';
-import 'src/win_game/win_game_screen.dart';
 
 Future<void> main() async {
   // To enable Firebase Crashlytics, uncomment the following lines and
@@ -115,63 +106,12 @@ final settingsControllerProvider = StateNotifierProvider<SettingsController, Set
   (ref) => SettingsController(persistence: LocalStorageSettingsPersistence())..loadStateFromPersistence(),
 );
 
-final audioControllerProvider = ChangeNotifierProvider<AudioController>((ref) => AudioController(ref)..initialize());
+final audioControllerProvider =
+    ChangeNotifierProvider<AudioController>((ref) => AudioController(ref.read)..initialize());
 
 final playerProgressProvider = StateNotifierProvider<PlayerProgress, int>(
   (ref) => PlayerProgress(LocalStoragePlayerProgressPersistence())..getLatestFromStore(),
 );
-
-final routerProvider = Provider<GoRouter>((ref) {
-  return GoRouter(
-    debugLogDiagnostics: true,
-
-    routes: [
-      GoRoute(path: '/', builder: (context, state) => const MainMenuScreen(key: Key('main menu')), routes: [
-        GoRoute(
-            path: 'play',
-            pageBuilder: (context, state) => buildMyTransition(
-                  child: const LevelSelectionScreen(key: Key('level selection')),
-                  color: ref.read(paletteProvider).backgroundLevelSelection,
-                ),
-            routes: [
-              GoRoute(
-                path: 'session/:level',
-                pageBuilder: (context, state) {
-                  final levelNumber = int.parse(state.params['level']!);
-                  final level = gameLevels.singleWhere((e) => e.number == levelNumber);
-                  return buildMyTransition(
-                    child: PlaySessionScreen(
-                      level,
-                      key: const Key('play session'),
-                    ),
-                    color: ref.read(paletteProvider).backgroundPlaySession,
-                  );
-                },
-              ),
-              GoRoute(
-                path: 'won',
-                pageBuilder: (context, state) {
-                  final map = state.extra! as Map<String, dynamic>;
-                  final score = map['score'] as Score;
-
-                  return buildMyTransition(
-                    child: WinGameScreen(
-                      score: score,
-                      key: const Key('win game'),
-                    ),
-                    color: ref.read(paletteProvider).backgroundPlaySession,
-                  );
-                },
-              )
-            ]),
-        GoRoute(
-          path: 'settings',
-          builder: (context, state) => const SettingsScreen(key: Key('settings')),
-        ),
-      ]),
-    ], // All the routes can be found there
-  );
-});
 
 class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
